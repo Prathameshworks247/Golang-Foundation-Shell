b5613112cb9d0c89fc7786585cdb764906e80d13
Prathameshworks247 <prathameshworks247@gmail.com>
2026-06-02 17:59:51 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,7 +6,14 @@ import (
 	"os"
 	"strings"
 )
-
+func Contains(slice []string, target string) bool {
+    for _, item := range slice {
+        if item == target {
+            return true
+        }
+    }
+    return false
+}
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	reader := bufio.NewReader(os.Stdin)
@@ -15,13 +22,21 @@ func main() {
 		fmt.Print("$ ")
 	
 		command, err := reader.ReadString('\n')
-
+		
 		command = strings.TrimSpace(command)
+		
+		builtin_commands := []string{"exit", "echo", "type"}
 
 		if command == "exit"{
 			break
 		}else if strings.HasPrefix(command, "echo"){
 			fmt.Println(command[5:])
+		}else if strings.HasPrefix(command, "type"){
+			if Contains(builtin_commands,command[5:]){
+				fmt.Println(command[5:] ,"is a shell builtin")
+			}else{
+				fmt.Println(command[5:] + ": command not found")
+			}
 		}else{
 			fmt.Println(command + ": command not found")
 		}
